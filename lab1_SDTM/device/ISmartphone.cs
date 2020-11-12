245096162771233323ccb20e9a64a7af96124767
Dina Ciorba <ciorbadina9@gmail.com>
2020-11-12 22:42:39 +0200

    organize creational_dp into packages

---
+++ lab1_SDTM/device/ISmartphone.cs
@@ -0,0 +1,41 @@
+﻿namespace lab1_SDTM
+{
+    // Abstract product for manufacturing Smartphone
+    public interface ISmartphone
+    {
+        string Name();
+    }
+
+    // Concrete products for their abstract products
+    public class XiaomiMiSmartphone : ISmartphone
+    {
+        public string Name()
+        {
+            return "Xiaomi Mi6X";
+        }
+    }
+
+    public class XiaomiRedmiSmartphone : ISmartphone
+    {
+        public string Name()
+        {
+            return "Xiaomi Redmi 9A";
+        }
+    }
+
+    public class HpSlateSmartphone : ISmartphone
+    {
+        public string Name()
+        {
+            return "HP Slate 6 VoiceTab II ";
+        }
+    }
+
+    public class HpEliteSmartphone : ISmartphone
+    {
+        public string Name()
+        {
+            return "HP Elite X3";
+        }
+    }
+}
